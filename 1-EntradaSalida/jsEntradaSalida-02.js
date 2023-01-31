c0d7f8a8b0a94dd765940d7ac4f8c381ce7ab0b0
Lautar055 <2lautarodandrea@gmail.com>
2023-01-31 11:16:27 -0300

    ejer2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,7 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar()
-{
-	alert("ok");
+function mostrar() {
+  let nombre = prompt("Cual es tu nombre");
+  alert(`Hola ${nombre} espero que estes bien`);
 }
-
