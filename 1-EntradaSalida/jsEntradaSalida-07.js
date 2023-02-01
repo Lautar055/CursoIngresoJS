4e5ff36299b584f4c33fdb105f86598a2cf093f1
Lautar055 <2lautarodandrea@gmail.com>
2023-02-01 12:25:06 -0300

    ejer 7 y 8

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -2,24 +2,55 @@
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
-ej.: "la Resta es 750"*/ 
-function sumar()
-{	
-	alert("ok");	
+ej.: "la Resta es 750"*/
+
+function sumar() {
+  let numUno = document.getElementById("txtIdNumeroUno").value;
+  let numDos = document.getElementById("txtIdNumeroDos").value;
+
+  numUno = parseInt(numUno);
+  numDos = parseInt(numDos);
+
+  let resultadoSuma = numUno + numDos;
+  let mensaje = `El resultado de la suma es ${resultadoSuma}.`;
+
+  alert(mensaje);
 }
 
-function restar()
-{
-	
+function restar() {
+  let numUno = document.getElementById("txtIdNumeroUno").value;
+  let numDos = document.getElementById("txtIdNumeroDos").value;
+
+  numUno = parseInt(numUno);
+  numDos = parseInt(numDos);
+
+  let resultadoResta = numUno - numDos;
+  let mensaje = `El resultado de la suma es ${resultadoResta}.`;
+
+  alert(mensaje);
+}
+function multiplicar() {
+  let numUno = document.getElementById("txtIdNumeroUno").value;
+  let numDos = document.getElementById("txtIdNumeroDos").value;
+
+  numUno = parseInt(numUno);
+  numDos = parseInt(numDos);
+
+  let resultadoMultipli = numUno * numDos;
+  let mensaje = `El resultado de la suma es ${resultadoMultipli}.`;
+
+  alert(mensaje);
 }
 
-function multiplicar()
-{ 
-	
-}
+function dividir() {
+  let numUno = document.getElementById("txtIdNumeroUno").value;
+  let numDos = document.getElementById("txtIdNumeroDos").value;
 
-function dividir()
-{
-	
-}
+  numUno = parseInt(numUno);
+  numDos = parseInt(numDos);
 
+  let resultadoDivi = numUno / numDos;
+  let mensaje = `El resultado de la suma es ${resultadoDivi}.`;
+
+  alert(mensaje);
+}
--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,9 +1,19 @@
-/*
+/* Lautaro D'Andrea
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
-function SacarResto()
-{
-	alert("ok");
+function SacarResto() {
+  let numDividendo = document.getElementById("txtIdNumeroDividendo").value;
+  let numDivisor = document.getElementById("txtIdNumeroDivisor").value;
+
+  numDividendo = parseInt(numDividendo);
+  numDivisor = parseInt(numDivisor);
+
+  let resultado = numDividendo / numDivisor;
+  resultado = parseInt(resultado);
+  let resto = numDividendo % numDivisor;
+
+  let mensaje = `El resultado de la divicion es ${resultado} y la divicion tine como resto ${resto}`;
+  alert(mensaje);
 }
