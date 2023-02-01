fa6149130960452cc59b95ca5a02db5aa50c177e
Lautar055 <2lautarodandrea@gmail.com>
2023-02-01 10:52:47 -0300

    ejer6

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,10 +1,11 @@
-/* Lautaro D'Andrea
+/*Lautaro D'Andrea
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar() {
-  nombreIngresado = document.getElementById("txtIdNombre").value;
-  edadIngresada = document.getElementById("txtIdEdad").value;
+  let nombreIngresado = document.getElementById("txtIdNombre").value;
+  let edadIngresada = document.getElementById("txtIdEdad").value;
 
-  mensaje = `Hola ${nombreIngresado} y que tenes ${edadIngresada} años`;
+  let mensaje = `Hola ${nombreIngresado} y que tenes ${edadIngresada} años`;
+
   alert(mensaje);
 }
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,9 +1,18 @@
-/*
+/* Lautaro D'Andrea
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
-function sumar()
-{
-	alert("ok");
-}
+function sumar() {
+  let numeroIngresadoUno = document.getElementById("txtIdNumeroUno").value;
+  let numeroIngresadoDos = document.getElementById("txtIdNumeroDos").value;
 
+  //numeroIngresadoUno = parseInt(Number(numeroIngresadoUno)); Funciona pero no es necesario el number, ya lo hace el parse
+  //numeroIngresadoDos = parseInt(Number(numeroIngresadoDos)); Funciona pero no es necesario el number, ya lo hace el parse
+
+  numeroIngresadoUno = parseInt(numeroIngresadoUno);
+  numeroIngresadoDos = parseInt(numeroIngresadoDos);
+
+  sumaNumeros = numeroIngresadoUno + numeroIngresadoDos;
+  let mensaje = `La suma de los numeros ingresados da ${sumaNumeros} (La suma no toma los decimales)`;
+  alert(mensaje);
+}
