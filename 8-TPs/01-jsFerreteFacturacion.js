fe287781a12273f6c2f6d6b0c4c0d8d20857a0e5
Lautar055 <2lautarodandrea@gmail.com>
2023-02-02 07:25:32 -0300

    tp 1

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -3,15 +3,34 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
+function Sumar() {
+  let numUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+  let numDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+  let numTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+  let numSum = numUno + numDos + numTres;
+
+  alert(numSum);
 }
-function Promedio () 
-{
-	
+
+function Promedio() {
+  let numUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+  let numDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+  let numTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+  let numSum = numUno + numDos + numTres;
+  let numProm = parseInt(numSum / 3);
+
+  alert(numProm);
 }
-function PrecioFinal () 
-{
-	
+
+function PrecioFinal() {
+  let numUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+  let numDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+  let numTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+  let numSum = numUno + numDos + numTres;
+  let numIVA = numSum * 1.21;
+
+  alert(numIVA);
 }
