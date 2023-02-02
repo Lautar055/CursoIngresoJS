066727ee0672636576704f1cfaff5592db6ab0b2
Lautar055 <2lautarodandrea@gmail.com>
2023-02-02 08:06:29 -0300

    numbre tp 1 y2

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,4 +1,4 @@
-/*1.	Para el departamento de facturación:
+/*1.	Para el departamento de facturación: Lautaro D'Andrea
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
