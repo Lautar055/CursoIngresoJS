d330ff4d72081732a2a59b0e60742d4b7e67262b
Lautar055 <2lautarodandrea@gmail.com>
2023-02-02 09:47:23 -0300

    10 bis

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -12,3 +12,24 @@ function mostrarAumento() {
   document.getElementById("txtIdResultado").value = parseInt(resul);
   //alert("ok");
 }
+
+/* Ejercicio 10.bis: Debemos lograr tomar el importe por ID. Lautaro D'Andrea
+Transformarlo a entero (parseInt), luego
+pedirle al usuario un % de descuento y mostrar el importe en el cuadro de texto "RESULTADO */
+
+function mostrarAumento() {
+  let importe = parseInt(document.getElementById("txtIdImporte").value);
+  let valores = parseInt(
+    prompt(
+      "De cuanto es el porcentaje, en caso de saer negativo introduzca un menos"
+    )
+  );
+
+  valores = 1 + valores / 100;
+
+  let resul = importe * valores;
+
+  //console.log(resul); hecho para controlar
+  document.getElementById("txtIdResultado").value = parseInt(resul);
+  //alert("ok");
+}
