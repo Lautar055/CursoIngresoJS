860a4486bc3c34076518d39d0352850519df835d
Lautar055 <2lautarodandrea@gmail.com>
2023-02-09 11:49:04 -0300

    sw 4

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,22 @@
-function mostrar()
-{
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+function mostrar() {
+  //tomo el mes
+  let mesDelAño = document.getElementById("txtIdMes").value;
+  let mensaje;
 
+  switch (mesDelAño) {
+    case "Febrero":
+      mensaje = `Este mes no tiene más de 29 días.`;
+      break;
 
+    case "Abril":
+    case "Junio":
+    case "Septiembre":
+    case "Noviembre":
+      mensaje = "Este mes tiene 30 días.";
+      break;
+    default:
+      mensaje = `Este mes tiene 31 días.`;
+  }
 
-}//FIN DE LA FUNCIÓN
+  alert(mensaje);
+} //FIN DE LA FUNCIÓN
