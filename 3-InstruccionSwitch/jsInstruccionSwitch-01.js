1c27c91705c2a60438e27162fc6c031e15c3cdd8
Lautar055 <2lautarodandrea@gmail.com>
2023-02-09 11:25:10 -0300

    sw 1

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,38 @@
-function mostrar()
-{
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+function mostrar() {
+  //tomo el mes
+  let mesDelAño = document.getElementById("txtIdMes").value;
+  let mensaje;
+  /* 
+  if (mesDelAño == "Enero") {
+    mensaje = `que comiences bien el año!!!.`;
+  } else if (mesDelAño == "Marzo") {
+    mensaje = `a clases!!!.`;
+  } else if (mesDelAño == "Julio") {
+    mensaje = `se vienen las vacaciones!!!.`;
+  } else if (mesDelAño == "Diciembre") {
+    mensaje = `Felices fiesta!!!.`;
+  } else {
+    mensaje = `${mesDelAño} es aburrido`;
+  }
+  alert(mensaje);
+ */
+  switch (mesDelAño) {
+    case "Enero":
+      mensaje = `que comiences bien el año!!!.`;
+      break;
+    case "Marzo":
+      mensaje = `a clases!!!.`;
+      break;
+    case "Julio":
+      mensaje = `se vienen las vacaciones!!!.`;
+      break;
+    case "Diciembre":
+      mensaje = `Felices fiesta!!!.`;
+      break;
 
+    default:
+      mensaje = `${mesDelAño} es aburrido`;
+  }
 
-
-}//FIN DE LA FUNCIÓN
+  alert(mensaje);
+} //FIN DE LA FUNCIÓN
