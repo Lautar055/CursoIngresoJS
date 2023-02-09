dcc13c18c1a457b78bacefd4b6b85c98a855760f
Lautar055 <2lautarodandrea@gmail.com>
2023-02-09 11:39:11 -0300

    sw 2

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,35 @@
-function mostrar()
-{
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+function mostrar() {
+  //tomo el mes
+  let mesDelAño = document.getElementById("txtIdMes").value;
+  let mensaje;
 
+  switch (mesDelAño) {
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+      mensaje = "Falta para el invierno.";
+      break;
 
+    case "Junio":
+    case "Julio":
+    case "Agosto":
+    case "Septiembre":
+      mensaje = "Abrigate que hace frio.";
+      break;
 
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+      mensaje = "Ya pasamos el frio, ahora calor!!!.";
+      break;
 
-}//FIN DE LA FUNCIÓN
+    case "Enero":
+    case "Febrero":
+      mensaje = "Ya pasamos el frio, ahora calor!!!.";
+      break;
+
+    default:
+      mensaje = `${mesDelAño} es aburrido`;
+  }
+  alert(mensaje);
+} //FIN DE LA FUNCIÓN
