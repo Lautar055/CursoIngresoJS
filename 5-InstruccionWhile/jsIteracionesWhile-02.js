4c89e401ceecd2b7602685de6503fd38b919e553
Lautar055 <2lautarodandrea@gmail.com>
2023-02-16 10:11:45 -0300

    while 2 bis bis

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -10,7 +10,8 @@ con números DESCENDENTES, desde el 10 al 1.*/
   }
 } */ //FIN DE LA FUNCIÓN
 
-// while 2 bis
+// while 2 bis Ingresar 5 números, y
+//determinar la cantidad de números que son mayores que 10 y menores 20 (inclusive en ambos casos)
 
 function mostrar() {
   let contador = 5;
@@ -27,5 +28,38 @@ function mostrar() {
   }
   alert(
     `Se ingresaron ${contadorSecundario} numeros mayores de 10 y menores de 20`
+  );
+}
+
+// while 2 bis bis Ingresar numeros hasta que el usuario quiera.
+//eterminar la cantidad de números que son mayores que 10 y menores 20
+//(inclusive en ambos casos). También el promedio de los
+//números que cumplen la condición anterior.
+
+function mostrar() {
+  let respuesta = "si";
+  let numeroIngresado;
+  let contadorCumplen = 0;
+  let sumaCumplen = 0;
+  let promedioCumplen;
+
+  while (respuesta == "si") {
+    numeroIngresado = parseInt(prompt("ingresa un numero"));
+    while (isNaN(numeroIngresado)) {
+      numeroIngresado = parseInt(prompt("ingresa un numero valido"));
+    }
+
+    if (numeroIngresado > 10 && numeroIngresado < 20) {
+      contadorCumplen = contadorCumplen + 1;
+      sumaCumplen = sumaCumplen + numeroIngresado;
+    }
+
+    respuesta = prompt("quieres ingresar mas numeros? si/no");
+  }
+
+  promedioCumplen = sumaCumplen / contadorCumplen;
+
+  alert(
+    `Se ingresaron ${contadorCumplen} numeros mayores de 10 y menores de 20 y el promedio de los unmeros es ${promedioCumplen}`
   );
 }
