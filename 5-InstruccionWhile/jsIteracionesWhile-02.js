91a8918269cef14a2e214d23b9a6497a0b48dea4
Lautar055 <2lautarodandrea@gmail.com>
2023-02-16 11:00:52 -0300

    correccion while 2 bis y bis bis

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -17,20 +17,25 @@ con números DESCENDENTES, desde el 10 al 1.*/
   let contador = 5;
   let numeroIngresado;
   let contadorSecundario = 0;
+  let contadorDi = 0;
+  let contadorVe = 0;
 
   while (contador > 0) {
     numeroIngresado = prompt("ingresa un numero");
 
-    if (numeroIngresado > 10 && numeroIngresado < 20) {
-      contadorSecundario = contadorSecundario + 1;
+    if (numeroIngresado > 10) {
+      contadorDi = contadorDi + 1;
+    }
+    if (numeroIngresado < 20) {
+      contadorVe = contadorVe + 1;
     }
     contador = contador - 1;
   }
   alert(
-    `Se ingresaron ${contadorSecundario} numeros mayores de 10 y menores de 20`
+    `De los numeros mayores de dies fueron ${contadorDi} y los menores de veinte fueros ${contadorVe}`
   );
-}
- */
+} */
+
 // while 2 bis bis Ingresar numeros hasta que el usuario quiera.
 //eterminar la cantidad de números que son mayores que 10 y menores 20
 //(inclusive en ambos casos). También el promedio de los
@@ -42,11 +47,20 @@ function mostrar() {
   let contadorCumplen = 0;
   let sumaCumplen = 0;
   let promedioCumplen;
+  let contadorDi = 0;
+  let contadorVe = 0;
 
   while (respuesta == "si") {
     numeroIngresado = parseInt(prompt("ingresa un numero"));
     while (isNaN(numeroIngresado)) {
       numeroIngresado = parseInt(prompt("ingresa un numero valido"));
+    }
+
+    if (numeroIngresado > 10) {
+      contadorDi = contadorDi + 1;
+    }
+    if (numeroIngresado < 20) {
+      contadorVe = contadorVe + 1;
     }
 
     if (numeroIngresado > 10 && numeroIngresado < 20) {
@@ -60,6 +74,9 @@ function mostrar() {
   promedioCumplen = sumaCumplen / contadorCumplen;
 
   alert(
-    `Se ingresaron ${contadorCumplen} numeros mayores de 10 y menores de 20 y el promedio de los unmeros es ${promedioCumplen}`
+    `Los numeros mayores de dies fueron ${contadorDi} y los menores de veinte fueron ${contadorVe}`
+  );
+  alert(
+    `Se ingresaron ${contadorCumplen} numeros mayores de 10 y menores de 20 y el promedio de los numeros es ${promedioCumplen}`
   );
 }
