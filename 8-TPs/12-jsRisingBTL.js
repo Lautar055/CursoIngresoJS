56c5c445ad7be243203bfc41f5dac03762c1406c
Lautar055 <2lautarodandrea@gmail.com>
2023-02-17 19:49:52 -0300

    TP 12

--- 8-TPs/12-jsRisingBTL.js
+++ 8-TPs/12-jsRisingBTL.js
@@ -10,26 +10,101 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
 function ComenzarIngreso() {
   //definicion de variables
   let edadIngresada = parseInt(prompt("Ingresa tu edad"));
-  let sexoIngresado;
-  let estadoCivilIngresado;
-  let sueldo;
-  let legajo;
+  let sexoIngresado = prompt("Ingresa tu sexo");
+  let estadoCivilIngresado = parseInt(
+    prompt(
+      "Ingresa un Estado Civil (1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos)"
+    )
+  );
+  let estadoCivil;
+  let sueldoIngresado = parseInt(prompt("Ingresa tu sueldo"));
+  let legajoIngresado = parseInt(prompt("Ingresa tu legajo"));
+  let nacionalidadIngresada = prompt(
+    "Ingresa tu nacionalidad “A” para argentinos, “E” para extranjeros, “N”"
+  );
   let nacionalidad;
 
-  for(edadIngresada){
-
-  while (isNaN(edadIngresada)) {
+  while (isNaN(edadIngresada) || edadIngresada < 18 || edadIngresada > 90) {
     edadIngresada = parseInt(prompt("Ingresa una edad valida"));
   }
-  if (edadIngresada > 18 && edadIngresada < 90) {
-    document.getElementById("txtIdEdad").value = edadIngresada;
-  }
+
+  while (
+    (!isNaN(sexoIngresado) || sexoIngresado != "f") &&
+    sexoIngresado != "m"
+  ) {
+    sexoIngresado = prompt("Ingresa un sexo valido");
   }
 
-
-}
-/*   while (edadIngresada < 18 || edadIngresada > 90) {
-    edadIngresada = parseInt(
-      prompt("Ingresa una edad valida, entre 18 y 90 años inclusive")
+  while (
+    isNaN(estadoCivilIngresado) ||
+    estadoCivilIngresado > 4 ||
+    estadoCivilIngresado < 0
+  ) {
+    estadoCivilIngresado = parseInt(
+      prompt(
+        "Ingresa un Estado Civil valido 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos"
+      )
     );
-  } */
+  }
+
+  switch (estadoCivilIngresado) {
+    case 1:
+      estadoCivil = "Soltero";
+      break;
+    case 2:
+      estadoCivil = "Casado";
+      break;
+    case 3:
+      estadoCivil = "Divorciado";
+      break;
+    case 4:
+      estadoCivil = "Viudo";
+      break;
+  }
+
+  while (sueldoIngresado < 8000) {
+    sueldoIngresado = parseInt(prompt("Ingresa tu sueldo verdadero"));
+  }
+
+  while (legajoIngresado < 1000 || legajoIngresado > 9999) {
+    legajoIngresado = parseInt(prompt("Ingresa tu legajo correctamente"));
+  }
+
+  while (
+    (!isNaN(nacionalidadIngresada) || nacionalidadIngresada != "A") &&
+    nacionalidadIngresada != "E" &&
+    nacionalidadIngresada != "N"
+  ) {
+    nacionalidadIngresada = prompt(
+      "Ingresa tu nacionalidad correctamente “A” para argentinos, “E” para extranjeros, “N”"
+    );
+  }
+
+  switch (nacionalidadIngresada) {
+    case "A":
+      nacionalidad = "Argentino";
+      break;
+    case "E":
+      nacionalidad = "Extrangeros";
+      break;
+    case "N":
+      nacionalidad = "Nacionalizados";
+      break;
+  }
+
+  document.getElementById("txtIdEdad").value = edadIngresada;
+  document.getElementById("txtIdSexo").value = sexoIngresado;
+  document.getElementById("txtIdEstadoCivil").value = estadoCivil;
+  document.getElementById("txtIdSueldo").value = sueldoIngresado;
+  document.getElementById("txtIdLegajo").value = legajoIngresado;
+  document.getElementById("txtIdNacionalidad").value = nacionalidad;
+}
+/* if (estadoCivilIngresado == 1) {
+  estadoCivilIngresado == "Soltero";
+} else if (estadoCivilIngresado == 2) {
+  estadoCivilIngresado == "Casado";
+} else if (estadoCivilIngresado == 3) {
+  estadoCivilIngresado == "Divorciado";
+} else if (estadoCivilIngresado == 4) {
+  estadoCivilIngresado == "Viudo";
+} */
