80837492672fd008ab78fee90a95d245e0b4c2b1
Lautar055 <2lautarodandrea@gmail.com>
2023-02-21 11:52:07 -0300

    5 bis

--- 6-InstruccionFor/jsIteracionesFor-05.js
+++ 6-InstruccionFor/jsIteracionesFor-05.js
@@ -26,18 +26,26 @@ function mostrar() {
   let banderaMal = true;
   let notaPeor;
   let sexoDelPeor;
-  let contDesaVar;
+  let contAproVar = 0;
+  let sumaNotas = 0;
+  let promedioNotas;
+  let contadorDesa = 0;
 
   for (i = 7; i > 0; i--) {
     nota = parseInt(prompt("escriba la nota del estudiantes"));
-    while (isNaN(nota)) {
-      nota = parseInt(prompt("tiene que ser un numero no texto"));
+    while (isNaN(nota) || nota > 10 || nota < 0) {
+      nota = parseInt(prompt("tiene que ser un numero valido"));
     }
 
     sexo = prompt("escriba el sexo del estudiante 'f' o 'm' o 'b' ");
-    while ((!isNaN(sexo) && sexo != "f") || sexo != "m" || sexo != "b") {
+    while (!isNaN(sexo)) {
       sexo = prompt(
-        "escriba un sexo valido para el estudiante 'f' o 'm' o 'b' "
+        "escriba un sexo valido para el estudiante no un numero 'f' o 'm' o 'b'  "
+      );
+    }
+    while (sexo != "f" && sexo != "m" && sexo != "b") {
+      sexo = prompt(
+        "escriba un sexo valido para el estudiante solo 'f' o 'm' o 'b' "
       );
     }
 
@@ -50,8 +58,24 @@ function mostrar() {
       sexoDelPeor = sexo;
     }
 
-    if ((sexo = "m" && nota < 4)) {
-      contDesaVar = contDesaVar + 1;
+    if (nota < 4) {
+      contadorDesa = contadorDesa + 1;
+    } else if (nota >= 6 && sexo == "m") {
+      contAproVar = contAproVar + 1;
     }
+
+    sumaNotas = sumaNotas + nota;
   }
+  promedioNotas = parseInt(sumaNotas / 7);
+
+  document.write(`El promedio de notas fue ${promedioNotas} <br>`);
+  document.write(
+    `La peor nota fue ${notaPeor} y el sexo del estudiante fue ${sexoDelPeor} <br>`
+  );
+  document.write(
+    `La cantidad de varones que su nota haya sido mayor o igual a 6 es ${contAproVar} <br>`
+  );
+  document.write(
+    `La cantidad de alumnos desaprobados fue ${contadorDesa} <br>`
+  );
 }
